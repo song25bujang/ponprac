886703ed83b46450400bc72ae6ff130f3f7d60d9
song25bujang <songjh3578@gmail.com>
2024-10-08 18:21:31 +0900

    init

---
+++ src/main/java/com/pony/ponyprac/PonypracApplication.java
@@ -0,0 +1,13 @@
+package com.pony.ponyprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonypracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PonypracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pony/ponyprac/PonypracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pony.ponyprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonypracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
